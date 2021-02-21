9a81f04c315795e15f7622b89dacf2d117de7803
rizfirman <rizfirman@gmail.com>
2021-02-21 23:27:28 +0700

    Create Navbar and Product Category Cards

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,17 @@
-import Link from 'next/link';
-import { connect } from 'react-redux';
-import { addItem } from 'state';
-import ROUTES from 'configs/routes';
-import { Button, Title } from 'components';
 
-const Home = ({ addItem, item }) => {
+import { Title, Navbar, Category } from 'components';
+
+const Home = ({  }) => {
   return (
     <div>
       <Title label="Belgian - Home" />
-      <h1>{item}</h1>
-      <Button />
-      <button onClick={addItem}>add item</button>
-      <Link href={ROUTES.CART}>cart</Link>
+      <Navbar />
+      <Category />
+      
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    item: state.item,
-  };
-};
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addItem: () => dispatch(addItem()),
-  };
-};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
