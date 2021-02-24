e43427a305b11a4f67c301834791ce9eefef99e7
Damayanti Putri Iswandari <damayantiputri449@gmail.com>
2021-02-25 01:58:11 +0700

    add cart page, cardbasket component

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,15 +1,51 @@
-import { Title } from 'components';
+import { Title, CardBasket } from 'components';
+import { product } from 'configs/product/product'
 
-const Cart = () => {
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      productList: product,
+    },
+  }
+}
+
+const Cart = ({productList}) => {
   return (
     <div>
+      <div>
       <Title label="Belgian - Cart" />
 
       <div className="cart">
-        <div className="cart_test">hallo</div>
+      <div className="container my-4 pt-3">
+        <h5 className="pt-3"><strong>My Cart</strong></h5>
+        <div className="card basket">
+        
+          {productList.map((productList) => (
+          <CardBasket key={productList.id} {...productList} />
+          ))}
+      
+        </div>
+      </div>
+      </div>
       </div>
     </div>
+        
+
   );
 };
+// const Cart = () => {
+//   return (
+//     <div>
+//       <Title label="Belgian - Cart" />
+
+//       <div className="cart">
+//       <div className="container my-4 pt-3">
+//         <h5 className="pt-3"><strong>My Cart</strong></h5>
+//       </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Cart;
