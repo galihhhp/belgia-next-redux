e8ae990fef2d90841c0332d9127ca97d341e9dc4
Kahlil Iman <kahlil.iman@gmail.com>
2021-03-03 21:29:29 +0700

    change class to className

--- src/state/reducers/cart-reducers.js
+++ src/state/reducers/cart-reducers.js
@@ -37,31 +37,32 @@ const cartReducers = (state = initialState, action) => {
   switch (action.type) {
     case ADD_ITEM:
       const item = state.products.find(prod => prod.id === action.payload.id)
-      const inCart = state.cart.find(item => item.id === action.payload.id ? true : false)
-      return { ...state,
+      const inCart = state.cart.find(item => item.id === action.payload.id)
+      return {
+        ...state,
         cart: inCart
-        ? state.cart.map(item =>
-          item.id === action.payload.id
-          ? {...item, qty: item.qty + 1}
-          : item)
-        : [...state.cart, {...item, qty: 1}],
+          ? state.cart.map(item =>
+            item.id === action.payload.id
+              ? { ...item, qty: item.qty + 1 }
+              : item)
+          : [...state.cart, { ...item, qty: 1 }],
       }
     case REMOVE_ITEM:
-      return{
+      return {
         ...state,
         cart: state.cart.filter((item) => item.id !== action.payload.id)
       }
     case ADJUST_QTY:
       return {
         ...state,
-        cart: state.cart.map((item)=>
+        cart: state.cart.map((item) =>
           item.id === action.payload.id
-          ? {...item, qty: action.payload.qty}
-          : item
-          ),
+            ? { ...item, qty: action.payload.qty }
+            : item
+        ),
       };
     case LOAD_CURRENT_ITEM:
-      return{
+      return {
         ...state,
         currentItem: action.payload,
       }
