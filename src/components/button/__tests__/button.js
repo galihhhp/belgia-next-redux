e48eb875820da4e781196450716bdd18569c217c
Kahlil Iman <kahlil.iman@gmail.com>
2021-03-24 18:53:29 +0700

    delete test

--- src/components/button/__tests__/button.js
+++
@@ -1,8 +0,0 @@
-import Button from 'components/button';
-import { render } from '@testing-library/react';
-
-test('Button renders correctly', () => {
-  const { container } = render(<Button type="primary">Submit</Button>);
-
-  expect(container).toMatchSnapshot();
-});
--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import React from 'react'
 
 const Button = ({
   label,
